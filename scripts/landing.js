c84844ea838c3f36b7f2f8f28adcb297308396a5
Romeo Palacios <romeopalacios@ymail.com>
2017-03-01 19:07:33 -0800

    landing.js from events

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
  
-     var points = document.getElementsByClassName('point');
+ var animatePoints = function(points) {
     
      var revealPoint = function(index) {
          points[index].style.opacity = 1;
@@ -13,3 +13,15 @@ var animatePoints = function() {
       }
      
 };
+window.onload = function() {
+      if(window.innerHeight > 950) {
+          animatePoints(pointsArray);
+      }
+    
+     
+     window.addEventListener('scroll', function(event) {
+         if (pointsArray[0].getBoundingClientRect().top <= 500){
+             animatePoints(pointsArray);   
+         }
+     });
+ }
