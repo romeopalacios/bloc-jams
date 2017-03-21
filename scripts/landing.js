c87ea4d51194c1b8875a8475149d0ee74aabbb2c
Romeo Palacios <romeopalacios@ymail.com>
2017-03-21 13:55:16 -0700

    refactored with jquery fixed

--- scripts/landing.js
+++ scripts/landing.js
@@ -12,22 +12,13 @@ $(window).load(function() {
         if ($(window).height() > 950) {
          animatePoints();
       } 
-<<<<<<< HEAD
-=======
 console.log($('.selling-points'))
->>>>>>> checkpoint-16-landingpage
 var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
     
      
 $(window).scroll(function(event) {
  if ($(window).scrollTop() >= scrollDistance) {
       animatePoints();
-<<<<<<< HEAD
-  }
-});
-});
-=======
     } 
   });
 });
->>>>>>> checkpoint-16-landingpage
